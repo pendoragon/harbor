8bdaf30da148a480a763145884dce9899b8a3a47
Penghao Cen <scorpiocph@gmail.com>
2016-12-08 20:11:51 +0800

    Sort tags by created time

--- src/ui/api/repository.go
+++ src/ui/api/repository.go
@@ -611,6 +611,22 @@ func newRepositoryClient(endpoint string, insecure bool, username, password, rep
 	return client, nil
 }
 
+type V1Compatibility struct {
+	Architecture    string      `json:"architecture"`
+	Config          interface{} `json:"config"`
+	Container       string      `json:"container"`
+	ContainerConfig interface{} `json:"container_config"`
+	Created         string      `json:"created"`
+	DockerVersion   string      `json:"docker_version"`
+	ID              string      `json:"id"`
+	OS              string      `json:"os"`
+	Parent          string      `json:"parent"`
+	Tag             string      `json:"tag"`
+	Throwaway       bool        `json:"throwaway"`
+}
+
+type V1CompatibilityList []V1Compatibility
+
 // TriggerSyncRepositoryLatestManifest
 func TriggerSyncRepositoryLatestManifest(repo_name string) error {
 	log.Debugf("TriggerSyncRepositoryLatestManifest, repo_name: %v", repo_name)
@@ -653,67 +669,86 @@ func TriggerSyncRepositoryLatestManifest(repo_name string) error {
 		return nil
 	}
 
-	sort.Strings(tags)
-	log.Debugf("get tags after sort: %v", tags)
-
 	// get manifest of latest tag
 	mediaTypes := []string{}
 	mediaTypes = append(mediaTypes, schema1.MediaTypeManifest)
 
-	latest_tag := tags[len(tags)-1]
+	var v1_compatibilities []V1Compatibility
 
-	_, mediaType, payload, err := rc.PullManifest(latest_tag, mediaTypes)
-	log.Debugf("mediaType: %v", mediaType)
-	if err != nil {
-		if regErr, ok := err.(*registry_error.Error); ok {
-			log.Errorf("registry_error: %v", regErr)
-			return nil
+	// sort by created time
+	for i, tag := range tags {
+		log.Debugf("tag[%v]: %v", i, tag)
+
+		_, mediaType, payload, err := rc.PullManifest(tag, mediaTypes)
+		log.Debugf("mediaType: %v", mediaType)
+		if err != nil {
+			if regErr, ok := err.(*registry_error.Error); ok {
+				log.Errorf("registry_error: %v", regErr)
+				continue
+			}
+
+			log.Errorf("error occurred while getting manifest of %s:%s: %v", repo_name, tag, err)
+			continue
 		}
 
-		log.Errorf("error occurred while getting manifest of %s:%s: %v", repo_name, latest_tag, err)
+		signed_manifest_v1 := new(schema1.SignedManifest)
+		err = signed_manifest_v1.UnmarshalJSON(payload)
+		if err != nil {
+			log.Errorf("UnmarshalJSON to get signed_manifest_v1 error, repo_name: %s, tag: %s", repo_name, tag)
+			continue
+		}
+
+		if len(signed_manifest_v1.History) <= 0 {
+			log.Errorf("signed_manifest_v1 has no history, repo_name: %s, tag: %s", repo_name, tag)
+			continue
+		}
+
+		var v1_compatibility V1Compatibility
+
+		// convert string to json
+		history0_v1_compatibility_str := signed_manifest_v1.History[0].V1Compatibility
+		err = json.Unmarshal([]byte(history0_v1_compatibility_str), &v1_compatibility)
+		if err != nil {
+			fmt.Println("error:", err)
+		}
+
+		v1_compatibility.Tag = tag
+
+		log.Debugf("v1_compatibility.Created: %v", v1_compatibility.Created)
+		v1_compatibilities = append(v1_compatibilities, v1_compatibility)
+	}
+
+	sort.Sort(V1CompatibilityList(v1_compatibilities))
+
+	if len(v1_compatibilities) <= 0 {
+		log.Errorf("v1_compatibilities not found for repo: %v", repo_name)
 		return nil
 	}
 
-	signed_manifest_v1 := new(schema1.SignedManifest)
-	err = signed_manifest_v1.UnmarshalJSON(payload)
-	if err != nil {
-		log.Errorf("UnmarshalJSON to get signed_manifest_v1 error, repo_name: %s, tag: %s", repo_name, latest_tag)
-		return nil
-	}
+	latest_tag := v1_compatibilities[0].Tag
+	latest_tag_ctime := v1_compatibilities[0].Created
 
-	if len(signed_manifest_v1.History) <= 0 {
-		log.Errorf("signed_manifest_v1 has no history, repo_name: %s, tag: %s", repo_name, latest_tag)
-		return nil
-	}
-
-	// convert string to json
-	type V1Compatibility struct {
-		Architecture    string      `json:"architecture"`
-		Config          interface{} `json:"config"`
-		Container       string      `json:"container"`
-		ContainerConfig interface{} `json:"container_config"`
-		Created         string      `json:"created"`
-		DockerVersion   string      `json:"docker_version"`
-		ID              string      `json:"id"`
-		OS              string      `json:"os"`
-		Parent          string      `json:"parent"`
-		Throwaway       bool        `json:"throwaway"`
-	}
-
-	var v1_compatibility V1Compatibility
-
-	history0_v1_compatibility_str := signed_manifest_v1.History[0].V1Compatibility
-	err = json.Unmarshal([]byte(history0_v1_compatibility_str), &v1_compatibility)
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-	log.Debugf("v1_compatibility.Created: %v", v1_compatibility.Created)
-
-	if err := dao.UpdateRepositoryLatestManifest(repo_name, latest_tag, v1_compatibility.Created, len(tags), "N/A"); err != nil {
+	if err := dao.UpdateRepositoryLatestManifest(repo_name, latest_tag, latest_tag_ctime, len(tags), "N/A"); err != nil {
 		log.Errorf("Error occurred in UpdateRepositoryLatestManifest: %v", err)
 		return err
 	}
 
 	return nil
+}
+
+// Len realize function of interface sort
+func (list V1CompatibilityList) Len() int {
+	return len(list)
+}
+
+// Less realize function of interface sort
+func (list V1CompatibilityList) Less(i, j int) bool {
+	return list[i].Created > list[j].Created
+}
+
+// Swap realize function of interface sort
+func (list V1CompatibilityList) Swap(i, j int) {
+	var temp V1Compatibility = list[i]
+	list[i] = list[j]
+	list[j] = temp
 }
